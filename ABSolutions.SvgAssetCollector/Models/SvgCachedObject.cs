0032000ec544ef469858340e9f2a34558bbd8ca5
Asif Bacchus <asif@asifbacchus.dev>
2024-10-23 00:45:28 -0600

    feat(models): Add Svg cached object model
    Add Microsoft.AspNetCore.Components NuGet package to support
    MarkupString struct. Create cached SVG object model.

---
+++ ABSolutions.SvgAssetCollector/Models/SvgCachedObject.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components;
+
+namespace ABSolutions.SvgAssetCollector.Models;
+
+/// <summary>
+///     SVG cached object with optional expiry date.
+/// </summary>
+public record SvgCachedObject
+{
+    public required string Filename { get; init; }
+    public required MarkupString Svg { get; init; }
+    public DateTime? Expiry { get; init; }
+
+    /// <summary>
+    ///     Override ToString() to provide relevant debugging output.
+    /// </summary>
+    /// <returns>String representation of this object.</returns>
+    public override string ToString()
+    {
+        return
+            $"Filename: {Filename} | MarkupString: {(string.IsNullOrWhiteSpace(Svg.Value) ? "<null>" : "<has value>")} | Expiry: {(Expiry.HasValue ? Expiry.Value.ToString("O") : "infinite")}";
+    }
+}
