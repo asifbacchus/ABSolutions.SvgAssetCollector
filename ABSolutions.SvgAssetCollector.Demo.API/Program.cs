b4fc214ec8b0d5194d4ba86d340b21d44463941c
Asif Bacchus <asif@asifbacchus.dev>
2024-10-24 03:40:07 -0600

    feat(demo-api): Add services
    Configure logging to use console JSON. Add named HTTP Client. Add
    SvgAssetCollector library services.

--- ABSolutions.SvgAssetCollector.Demo.API/Program.cs
+++ ABSolutions.SvgAssetCollector.Demo.API/Program.cs
@@ -1,4 +1,30 @@
+using System.Text.Json;
+using ABSolutions.SvgAssetCollector.DependencyInjection;
+using ABSolutions.SvgAssetCollector.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging(logBuilder => logBuilder.AddJsonConsole(opts =>
+    {
+        opts.IncludeScopes = true;
+        opts.TimestampFormat = "[HH:mm:ss] ";
+        opts.JsonWriterOptions = new JsonWriterOptions
+        {
+            Indented = true
+        };
+    })
+);
+
+builder.Services.AddHttpClient(
+    builder.Configuration.GetRequiredSection(SvgAssetCollectorConfiguration.AppSettingsKey)
+        .Get<SvgAssetCollectorConfiguration>()?.HttpClientName ?? "SvgAssetCollectorClient",
+    client =>
+    {
+        client.DefaultRequestHeaders.Add("Accept", "image/svg+xml");
+        client.DefaultRequestHeaders.UserAgent.ParseAdd("SvgAssetCollector");
+    });
+builder.Services.AddSvgAssetCollector(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
